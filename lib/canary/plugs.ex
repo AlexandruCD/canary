22832e2f86e7497722cc21a76eedda535a41936c
Alexandru Creanga <alexandru.creanga@assist.ro>
2017-04-26 15:01:03 +0300

    next step 3

--- lib/canary/plugs.ex
+++ lib/canary/plugs.ex
@@ -87,6 +87,7 @@ defmodule Canary.Plugs do
   ```
   """
   def authorize(conn, opts) do
+     IO.inspect opts[:actions]
     if action_valid?(conn, opts) && opts[:actions] do
       conn
       |> authorize_action(opts) 
